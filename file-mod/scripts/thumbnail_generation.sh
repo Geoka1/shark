8bf50fa0b519d1138f9139dd948f09f0d4fb7fc6
Geoka1 <geoka001@gmail.com>
2025-06-03 19:22:17 +0300

    fix

--- file-mod/scripts/thumbnail_generation.sh
+++ file-mod/scripts/thumbnail_generation.sh
@@ -4,13 +4,4 @@
 input="$1"
 dest="$2"
 
-mkdir -p "$dest"
-
-for img in "$input"/*.jpg; do
-    {
-        filename=$(basename "$img" .jpg)
-        convert "$img" -thumbnail 100x100 "$dest/${filename}.gif"
-    } &
-done
-
-wait
+mogrify  -format gif -path "$dest" -thumbnail 100x100 $input/*.jpg
